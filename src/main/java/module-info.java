4eb62049003d211ba51a1f702ef0c9b64da03c6e
Luis <ls1224268@swccd.edu>
2026-05-24 22:45:05 -0700

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module blankjavafx.finalprojectfx5 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens blankjavafx.finalprojectfx5 to javafx.fxml;
-    exports blankjavafx.finalprojectfx5;
-}
